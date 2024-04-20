5f85b08f3cf1950216475d788bf26b174d0849ad
Sanjay S <sanjays@Sanjays-MacBook-Air.local>
2024-04-20 16:57:45 +0530

    dp fibonacci

--- usaco/completeSearchWithRecursion/fibonacci.java
+++ usaco/completeSearchWithRecursion/fibonacci.java
@@ -1,12 +1,16 @@
 package usaco.completeSearchWithRecursion;
 
+import java.util.Arrays;
 import java.util.Scanner;
-// without
+// dp
 public class fibonacci {
     static int cnt =0;
+    static int[] cache = new int[50];
+
     public static void main(String[] args) {
 //        Scanner sc = new Scanner(System.in);
 //        int n= sc.nextInt();
+        Arrays.fill(cache,-1);
         System.out.println(fibo(6));
         System.out.println("Number of times fib called = "+ cnt);
 
@@ -15,9 +19,11 @@ public class fibonacci {
     public static int fibo(int n){
         cnt++;
         if(n<2) return n;
-        else{
-            return fibo(n-1)+fibo(n-2);
-        }
+        if(cache[n] !=-1) return cache[n];
+
+        cache[n]= fibo(n-1)+fibo(n-2);
+        return cache[n];
+
     }
 
 }
